2669c11b45be423149d199c09489569eeabcd3ab
om-dev007 <officialomnarayan@gmail.com>
2026-03-22 14:48:25 +0530

    cors update

--- src/app.js
+++ src/app.js
@@ -11,7 +11,13 @@ import wishlistRouter from './routes/wishlist.routes.js';
 const app = e();
 
 app.use(e.json());
-app.use(cors({origin: "http://localhost:5173",credentials: true}));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://velnixa.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(cookieParser())
 app.use("/api/auth", authRoutes);
 app.use("/user/profile", userRoutes);
