d9ad89fbe52eaec769ef6d4b96552202ad6d1256
om-dev007 <officialomnarayan@gmail.com>
2026-04-30 01:46:46 +0530

    origin update

--- src/app.ts
+++ src/app.ts
@@ -6,16 +6,29 @@ import router from './routes/index';
 
 const app = e();
 
-const corsOption = {
-    origin: ["https://velnixa.vercel.app/"],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}
+const allowedOrigins = [
+  "https://velnixa.vercel.app",
+  "http://localhost:5173"
+];
 
-app.use(cors(corsOption));
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("CORS not allowed"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
 app.use(e.json());
-app.use(cookieParser())
+app.use(cookieParser());
+
 app.use("/", HomeRoutes);
 app.use("/api", router);
 
